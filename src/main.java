524ad1926601d04ce3053ae4512fe2520c27dcb6
AbdullahHAboElkeir <bedohisham234@gmail.com>
2026-04-26 22:54:46 +0300

    up
    up

--- src/main.java
+++ src/main.java
@@ -2,6 +2,6 @@ class main {
     public static void main(String[] args) 
     {
         System.out.println("Hello World!");
-        System.out println("aaaaaaa");
+        System.out. println("aaaaaaa");
     }
 }   
